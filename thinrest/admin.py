7290dfd5c028c4c968d9c485b2b7ed3f46f89da4
Anurag Agarwal <anurag26@ambersoft.in>
2015-10-04 19:42:33 +0530

    Removed django server files

--- thinrest/admin.py
+++
@@ -1,9 +0,0 @@
-"""
-admin.py for enabling admin functionality on models
-"""
-from django.contrib import admin
-
-from .models import Employee, SystemSetting
-
-admin.site.register(Employee)
-admin.site.register(SystemSetting)
