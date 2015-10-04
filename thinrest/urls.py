159b28bb0379d076f493773b1f0c40382f3f5e71
Anurag Agarwal <anurag26@ambersoft.in>
2015-10-04 19:42:11 +0530

    Removed default commented lines

--- thinrest/urls.py
+++ thinrest/urls.py
@@ -2,10 +2,6 @@ from django.conf.urls import patterns, include, url
 
 from tastypie.api import NamespacedApi
 
-# Uncomment the next two lines to enable the admin:
-from django.contrib import admin
-admin.autodiscover()
-
 from .api import EmployeeResource, SettingResource
 
 v1_api = NamespacedApi(api_name='v1', urlconf_namespace='')
@@ -14,13 +10,4 @@ v1_api.register(SettingResource())
 
 urlpatterns = patterns('',
     url(r'^api/', include(v1_api.urls)),
-    # Examples:
-    # url(r'^$', 'thinrest.views.home', name='home'),
-    # url(r'^thinrest/', include('thinrest.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
 )
